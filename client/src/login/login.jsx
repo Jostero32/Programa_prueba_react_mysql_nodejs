1160e9e716c8d46a9e24e04fee2d4d1d0f07f464
Jostero32 <jostero32@gmail.com>
2024-05-19 16:22:11 -0500

    se agrego css a el formulario de login

--- client/src/login/login.jsx
+++ client/src/login/login.jsx
@@ -1,56 +1,79 @@
-import React, { useState} from 'react'
-import Axios from 'axios';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import Axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../login/login.css";
 
 function Login() {
-  const [email,setEmail]= useState("");
-  const [clave,setClave]= useState("");
-  const navegar=useNavigate();
+  const [email, setEmail] = useState("");
+  const [clave, setClave] = useState("");
+  const navegar = useNavigate();
 
-Axios.defaults.withCredentials=true;
+  Axios.defaults.withCredentials = true;
 
-useEffect(() => {
-  Axios.get("http://localhost:3001")
-    .then((res) => {
-      if (res.data.valid) {
-        navegar('/home');
-      } else {
-        navegar("/");
-      }
-    })
-    .catch((err) => console.log(err));
-}, []);
+  useEffect(() => {
+    Axios.get("http://localhost:3001")
+      .then((res) => {
+        if (res.data.valid) {
+          navegar("/home");
+        } else {
+          navegar("/");
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-function validar(event){
-event.preventDefault();
-Axios.post('http://localhost:3001/login',{email,clave}).then(
-res=>{
-  if(res.data.length>0){
-    navegar('/home');
-  }else{
-    alert('Usuario o Contraseña erroneos.');
+  function validar(event) {
+    event.preventDefault();
+    Axios.post("http://localhost:3001/login", { email, clave })
+      .then((res) => {
+        if (res.data.length > 0) {
+          navegar("/home");
+        } else {
+          alert("Usuario o Contraseña erroneos.");
+        }
+      })
+      .catch((err) => console.log(err));
   }
-}
-  
-).catch(err=>console.log(err));
-}
 
   return (
+    <>
     <div>
-      <form onSubmit={validar}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" placeholder='Ingresa tu Email' onChange={(event) => setEmail(event.target.value)}/>
+    <img className="banner" src="https://www.uta.edu.ec/v3.2/uta/images/header.png" alt="" />
+  </div>
+    <div className="d-flex vh-50 login-container justify-content-center align-items-center">
+      <div className="p-3 w-25">
+        <div className="form-login">
+          <form onSubmit={validar}>
+            <div className="input-login">
+              <label htmlFor="email" className="label-login">Email</label><br />
+              <input
+                type="text"
+                placeholder="Ingresa tu Email"
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <div className="input-login">
+              <label htmlFor="password" className="label-login">Contraseña</label><br />
+              <input
+                type="password"
+                placeholder="Ingresa tu Contraseña"
+                onChange={(event) => setClave(event.target.value)}
+              />
+            </div>
+            <div className="boton-iniciarsesion ">
+              <button className="btn btn-secondary">Iniciar Sesion</button>
+            </div>
+          </form>
         </div>
-        <div>
-        <label htmlFor="password">Email</label>
-          <input type="password" placeholder='Ingresa tu Contraseña' onChange={(event) => setClave(event.target.value)}/>
-        </div>
-        <button>Iniciar Sesion</button>
-      </form>
+      </div>
     </div>
-  )
+    <hr />
+    <footer>
+      <p>2024 - UTA</p>
+    </footer>
+    </>
+  );
 }
 
-export default Login
+export default Login;
